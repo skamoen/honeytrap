5afb1fde2bf62aece5206fef20053d57d68d0d99
Sille Kamoen <sille.kamoen@gmail.com>
2017-10-27 16:19:20 +0200

    Add exact negotiaton matcher

--- services/telnet/matcher/negotiation.go
+++ services/telnet/matcher/negotiation.go
@@ -1,21 +1,63 @@
 package matcher
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/honeytrap/honeytrap/services/telnet"
 )
 
 var (
-	negotiations []*telnet.Negotiation
+	negotiations [][]*negotiateCommand
 )
 
-type negotiateCommands struct {
+type negotiateCommand struct {
+	option      byte
+	command     byte
+	subcommands []byte
 }
 
 func SubmitNegotiation(n *telnet.Negotiation, id int) {
-	negotiations = append(negotiations, n)
+	pn := parseCommands(n)
+	seenBefore := checkNegotiation(pn)
+	if seenBefore {
+		fmt.Println("Negotiation seen before!")
+	} else {
+		negotiations = append(negotiations, pn)
+	}
 }
 
-func parseCommands(n *telnet.Negotiation) []negotiateCommands {
+func parseCommands(n *telnet.Negotiation) []*negotiateCommand {
+	commands := make([]*negotiateCommand, 0)
 
-	return nil
+	split := bytes.Split(n.Bytes, []byte{telnet.IAC})
+
+	for _, c := range split {
+		if len(c) > 0 {
+			command := &negotiateCommand{
+				option:  c[0],
+				command: c[1],
+			}
+
+			if c[1] == telnet.SubNegotiationStart || len(c) > 2 {
+				command.subcommands = c[2:]
+			}
+			commands = append(commands, command)
+		}
+	}
+
+	return commands
+}
+
+func checkNegotiation(n []*negotiateCommand) bool {
+	for _, negotiation := range negotiations {
+		for index, command := range negotiation {
+			if command.option == n[index].option &&
+				command.command == n[index].command &&
+				bytes.Compare(command.subcommands, n[index].subcommands) == 0 {
+				return true
+			}
+		}
+	}
+	return false
 }
